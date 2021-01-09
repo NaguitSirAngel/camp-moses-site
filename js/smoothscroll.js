c09535d6626eabe17a0138f660364d9c6b8ed5bd
Sir Angel Naguit <angelnaguit@icloud.com>
2021-01-09 01:19:52 -0500

    Update smoothscroll.js

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -298,5 +298,6 @@ var ischrome = /chrome/.test(navigator.userAgent.toLowerCase());
 
 if (ischrome) {
   ssc_addEvent("mousedown", ssc_mousedown);
+  // ssc_addEvent("mousewheel", ssc_wheel);
   ssc_addEvent("load", ssc_init)
 }
